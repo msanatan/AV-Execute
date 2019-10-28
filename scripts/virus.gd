a08f18e1e259d733b0e0c9f07a38773c19e44c9e
Marcus Sanatan <msanatan@gmail.com>
2019-10-28 19:53:15 -0400

    Add standard virus properties like speed and direction
    Each inherited virus can define their own speed range start direction.
    These properties may change once we decide more appropriate enemy behaviour.

--- scripts/virus.gd
+++ scripts/virus.gd
@@ -1,6 +1,17 @@
 extends RigidBody2D
 
 export (int) var health
+export (int) var min_speed
+export (int) var max_speed
+var direction : int
 
 func _ready():
 	pass
+
+
+func set_start_position(start_pos: Vector2):
+	position = start_pos
+
+
+func set_direction(dir: int):
+	direction = dir
