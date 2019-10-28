c9fb756b3bdc346c0bc6cc79a2885a9ad2da675b
Marcus Sanatan <msanatan@gmail.com>
2019-10-28 16:45:14 -0400

    Add player script to move around
    This is a basic script that gets 8-way movement.
    The angle calculation is a bit primitive but it works...

---
+++ scripts/player.gd
@@ -0,0 +1,41 @@
+extends Area2D
+
+export (int) var SPEED = 300
+export var angle = 0
+
+func _ready():
+	pass
+
+
+func _process(delta):
+	var velocity = Vector2()
+	var up : bool = false
+	var down : bool = false
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+		angle = 0
+		up = true
+	elif Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+		angle = 180
+		down = true
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+		if up:
+			angle -= 45
+		elif down:
+			angle += 45
+		else:
+			angle = -90
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+		if up:
+			angle += 45
+		elif down:
+			angle -= 45
+		else:
+			angle = 90
+	
+	velocity = velocity.normalized()
+	rotation_degrees = angle
+	position += velocity * SPEED * delta
