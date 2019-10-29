410dc9f703349846b67f04e81463deb0e786f030
Marcus Sanatan <msanatan@gmail.com>
2019-10-29 00:28:51 -0400

    Fire lasers on space bar (incomplet)
    Took a while, but the best solution was for me to store the last changed
    direction of the player, and make that the velocity of the laser.
    
    There's still a bug, when a player is moving the lasers aren't firing. Will
    work on that tomorrow

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,10 @@
 extends Area2D
 
-export (int) var SPEED = 300
+export (PackedScene) var player_laser
+export (int) var speed = 300
 export (int) var health = 100
 var angle : int = 0
+var direction : Vector2 = Vector2(0, -1)
 
 func _ready():
 	pass
@@ -38,8 +40,18 @@ func _process(delta):
 			angle = 90
 	
 	velocity = velocity.normalized()
+	if velocity.length() != 0:
+		direction = velocity
+	
 	rotation_degrees = angle
-	position += velocity * SPEED * delta
+	position += velocity * speed * delta
+	
+	if Input.is_action_pressed("ui_accept"):
+		var laser = player_laser.instance()
+		laser.set_start_position(position)
+		laser.velocity = direction
+		laser.angle = deg2rad(angle)
+		get_tree().get_root().add_child(laser)
 
 
 func set_start_position(start_position : Vector2):
