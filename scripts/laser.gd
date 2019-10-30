7ed8579d88a57dad3f9dc80d86ed8c1ade53d87e
Marcus Sanatan <msanatan@gmail.com>
2019-10-29 21:23:15 -0400

    Set rotation of laser with function
    Instead of providing an angle, expose the rotation of the laser.
    This is better than doing it during _process as the laser is now rotated to the
    direction it's facing before it's shot. So it continues to look like one,
    consistent shot

--- scripts/laser.gd
+++ scripts/laser.gd
@@ -1,7 +1,6 @@
 extends Area2D
 
 export (Vector2) var velocity
-export (int) var angle
 export (int) var speed
 
 func _ready():
@@ -12,8 +11,11 @@ func _process(delta):
 	position += velocity * speed * delta
 
 
-func set_start_position(start_position : Vector2):
+func set_start_position(start_position: Vector2):
 	position = start_position
+
+func set_rotation(angle: float):
+	rotation = angle
 
 
 func _on_VisibilityNotifier2D_screen_exited():
