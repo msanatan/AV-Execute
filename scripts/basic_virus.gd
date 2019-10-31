a385fdac482be550b3a4ff250fffe44bef2bf30b
Marcus Sanatan <msanatan@gmail.com>
2019-10-30 22:05:29 -0400

    Shoot lasers every 2 seconds for basic enemy
    Every laser goes in 4 directions from the virus' origin. We also use the shoot
    timer, so the virus moves roughly twice before shooting

--- scripts/basic_virus.gd
+++ scripts/basic_virus.gd
@@ -1,19 +1,22 @@
 extends "res://scripts/virus.gd"
 
+export (PackedScene) var basic_virus_laser
 var can_move: bool
 var velocity: Vector2
 
 func _ready():
 	can_move = true
-	rotation = direction
 
 func _process(delta):
 	if can_move:
-		move_and_collide(Vector2(speed, 0).rotated(direction))
+		move_and_collide(Vector2(speed, 0).rotated(angle))
 		can_move = false
 		$MoveTimer.start()
 	else:
 		move_and_collide(Vector2())
+	
+	if can_shoot:
+		shoot()
 
 func _on_ShootTimer_timeout():
 	can_shoot = true
@@ -21,3 +24,17 @@ func _on_ShootTimer_timeout():
 
 func _on_MoveTimer_timeout():
 	can_move = true
+
+
+func shoot():
+	var rotation_offset: float = 0
+	for i in range(4):
+		var laser = basic_virus_laser.instance()
+		laser.set_start_position(position)
+		var laser_angle = rotation + rotation_offset
+		laser.velocity = Vector2(0, 1).rotated(laser_angle)
+		laser.set_rotation(laser_angle)
+		get_tree().get_root().add_child(laser)
+		rotation_offset += PI / 2
+	can_shoot = false
+	$ShootTimer.start()
