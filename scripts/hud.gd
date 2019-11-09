0231383a33d82632c38cdac0a071e6ac5614f7d6
Marcus Sanatan <msanatan@gmail.com>
2019-11-09 16:55:28 -0400

    Health bar is gradually reduced
    Using a tween makes that look so much better than a drastic drop.
    
    Btw, tweens on numbers work with floats - not ints :-/

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -4,14 +4,28 @@ onready var ScoreLabel = $GUI/VBoxContainer/TopHUD/ScoreLabel
 onready var HPLabel = $GUI/VBoxContainer/TopHUD/HealthBars/HealthLabel
 onready var HPGauge = $GUI/VBoxContainer/TopHUD/HealthBars/Gauge
 onready var Countdown = $GUI/VBoxContainer/CenterContainer/Countdown
+onready var tween = $Tween
+export (int) var player_max_health
+var animated_health : float = 0
 
 func _ready():
-	pass
+	HPGauge.max_value = player_max_health
+	animated_health = player_max_health
+
+
+func _process(delta):
+	var rounded_health : int = round(animated_health)
+	HPLabel.text = "HP:      " + str(rounded_health)
+	HPGauge.value = rounded_health
 
 
 func update_player_health(health: int):
-	HPLabel.text = "HP:      " + str(health)
-	HPGauge.value = health
+	tween.interpolate_property(self, "animated_health", animated_health,
+		health, 0.6, Tween.TRANS_LINEAR, Tween.EASE_IN)
+	if not tween.is_active():
+		tween.start()
+	print("health ", health)
+	print("animated_health ", animated_health)
 
 
 func update_score(score: int):
